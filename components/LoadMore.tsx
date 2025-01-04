c376472aa243da1caaaf71cc5fcb573d1233177e
Adlichalbi <adlichalbi6612@gmail.com>
2025-01-04 11:28:54 +0100

    feat:add infinite scroll

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const pageNumber = useRef(1); // Using ref to track page number
+  useEffect(() => {
+    if (inView) {
+      const currentPage = pageNumber.current;
+
+      fetchAnime(currentPage).then((res) => {
+        setData([...data, ...res]);
+        pageNumber.current = currentPage + 1; // Increment page number
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
